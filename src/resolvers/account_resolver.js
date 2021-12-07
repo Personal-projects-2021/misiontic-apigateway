a05cfb15aeb6b415c24b1c7dd481e07240fd6d2c
Anderson DZ <anderson9daza@gmail.com>
2021-12-07 16:31:59 -0500

    Account resolver

--- src/resolvers/account_resolver.js
+++ src/resolvers/account_resolver.js
@@ -1,9 +1,9 @@
 const accountResolver = {
     Query: {
         // traer nombre de la consulta definida en typedef
-        accountByUsername: (_, {username}, {dataSource}) => {
+        accountByUsername: async (_, {username}, {dataSource}) => {
             // esta función se comunica con el ms, para ello el datasource
-            
+            return await dataSource.accountByUsername(username);
         }
     }
 }
